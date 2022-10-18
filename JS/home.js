4d2293ccd1820e1c24270574dcbe3a0027d9fdaf
Amankumarsinghdeveloper <amankumarsing956@gmail.com>
2022-10-18 22:44:16 +0530

    update

--- JS/home.js
+++ JS/home.js
@@ -51,3 +51,80 @@ window.onload = function () {
 
 };
 
+
+
+let tempicon = document.getElementById("temp-icon");
+let tempvalue = document.getElementById("temp-value");
+let iconfile;
+
+window.addEventListener("load", () => {
+
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+
+
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+
+
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=dab3af44de7d24ae7ff86549334e45bd`
+
+      fetch(api).then((response) => {
+
+        return response.json();
+
+
+      })
+
+        .then(data => {
+
+          const { feels_like } = data.main;
+          const { id} = data.weather[0];
+
+
+          tempvalue.textContent = Math.round(feels_like - 273);
+          if (id < 300 && id > 200) {
+            tempicon.src = "./icons/thunderstorm.svg"
+          }
+          else if (id < 400 && id > 300) {
+            tempicon.src = "./icons/cloud-solid.svg"
+          }
+          else if (id < 600 && id > 500) {
+            tempicon.src = "./icons/rain.svg"
+          }
+          else if (id < 700 && id > 600) {
+            tempicon.src = "./icons/snow.svg"
+          }
+          else if (id < 800 && id > 700) {
+            tempicon.src = "./icons/clouds.svg"
+          }
+          else if (id == 800) {
+            tempicon.src = "./icons/clouds-and-sun.svg"
+          }
+
+
+
+
+
+          console.log(data);
+
+
+        })
+
+
+
+    }
+
+
+
+    )
+  }
+
+
+})
